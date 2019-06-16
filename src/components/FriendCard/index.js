5e895e838665670616d3e73662b3511595f91406
Facundo Rodriguez <facurodri91@gmail.com>
2019-06-16 16:22:08 -0400

    fixed up game and styles

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -9,13 +9,8 @@ function FriendCard(props) {
         <img alt={props.name} src={props.image} />
       </div>
       <div className="content">
-        {/* <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-        </ul> */}
       </div>
-      {/* <span className="remove">𝘅</span> */}
+     
     </div>
   );
 }
