15c852850a1171a025d3cdf8ab8f5ac76722a6c4
Facundo Rodriguez <facurodri91@gmail.com>
2019-06-26 13:41:02 -0400

    fixed sizes again

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = props => {
   return (
     <div className="jumbotron">
       <a className="navbar-brand" href="#">
-        <h1>Clicky Corgis Shuffle Game</h1>
+        <h1>Shuffle Dog Game</h1>
       </a>
       <h4>
         Click on a giphy to earn a point, but don't click on the same giphy
